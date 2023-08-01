703e2f322c9c3015eb8c3475c27eb86c9f7d061f
TearsOprah <qmeshokp@mail.ru>
2023-08-01 13:57:23 +0300

    feat: верстка Schedule

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Intro from "../Intro/Intro.jsx";
+import Schedule from "../Schedule/Schedule.jsx";
 
 function App() {
 
   return (
     <>
       <Intro />
+      <Schedule />
     </>
   )
 }
---
+++ src/components/Schedule/Schedule.jsx
@@ -0,0 +1,81 @@
+import './Schedule.css';
+import scheduleImg from '../../assets/schedule-image.png';
+
+const Schedule = () => {
+  return (
+    <section className={'schedule'}>
+
+      <ul className={'schedule__list'}>
+        <li className={'schedule__list-item'}>
+          <p className={'schedule__day'}>Woensdag 12/01</p>
+          <div className={'schedule__menu'}>
+            <p className={'schedule__dish'}>Salade met Serrano</p>
+            <div className={'circle-block'}>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+            </div>
+            <p className={'schedule__dish'}>Steak Tartaar</p>
+          </div>
+        </li>
+
+        <li className={'schedule__list-item'}>
+          <p className={'schedule__day'}>Donderdag 13/01</p>
+          <div className={'schedule__menu'}>
+            <p className={'schedule__dish'}>Toast Roquefort</p>
+            <div className={'circle-block'}>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+            </div>
+            <p className={'schedule__dish'}>Penne met Kip en Prei</p>
+          </div>
+        </li>
+
+        <li className={'schedule__list-item'}>
+          <p className={'schedule__day'}>Vrijdag 14/01</p>
+          <div className={'schedule__menu'}>
+            <p className={'schedule__dish'}>Verrassing van de Chef</p>
+            <div className={'circle-block'}>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+            </div>
+            <p className={'schedule__dish'}>Penne met Kip en Prei</p>
+          </div>
+        </li>
+
+        <li className={'schedule__list-item'}>
+          <p className={'schedule__day'}>Vrijdag 15/01</p>
+          <div className={'schedule__menu'}>
+            <p className={'schedule__dish'}>Verrassing van de Chef</p>
+            <div className={'circle-block'}>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+            </div>
+            <p className={'schedule__dish'}>Penne met Kip en Prei</p>
+          </div>
+        </li>
+
+        <li className={'schedule__list-item'}>
+          <p className={'schedule__day'}>Vrijdag 16/01</p>
+          <div className={'schedule__menu'}>
+            <p className={'schedule__dish'}>Verrassing van de Chef</p>
+            <div className={'circle-block'}>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+              <div className={'circle'}></div>
+            </div>
+            <p className={'schedule__dish'}>Penne met Kip en Prei</p>
+          </div>
+        </li>
+      </ul>
+
+      <img className={'schedule__image'} src={scheduleImg} alt={'photo Tzeezotje'} />
+
+    </section>
+  )
+}
+
+export default Schedule;
