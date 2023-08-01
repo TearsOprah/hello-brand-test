39aba3641301741c6a60887a0e473791fe6525da
TearsOprah <qmeshokp@mail.ru>
2023-08-01 21:15:35 +0300

    feat: открытие модалки

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,7 +1,43 @@
 import Header from "../Header/Header.jsx";
+import { useState, useEffect } from 'react';
 import './Intro.css';
 
 const Intro = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleEscapeKeyPress = (e) => {
+    if (e.key === 'Escape') {
+      handleCloseModal();
+    }
+  };
+
+  const handleOutsideClick = (e) => {
+    if (e.target.classList.contains('modal-overlay')) {
+      handleCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.addEventListener('keydown', handleEscapeKeyPress);
+    } else {
+      document.removeEventListener('keydown', handleEscapeKeyPress);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscapeKeyPress);
+    };
+  }, [isModalOpen]);
+
   return (
     <div className={'intro'}>
       <Header />
@@ -9,7 +45,7 @@ const Intro = () => {
         <p className={'intro__time'}>OPEN EVERYDAY 6AM - 9PM</p>
         <h1 className={'intro__title'}>A bad day with lunch is better than a good day without it.</h1>
         <div className={'links-block'}>
-          <button className={'intro__button'}>Reserve a table</button>
+          <button className={'intro__button'} onClick={handleOpenModal}>Reserve a table</button>
           <ul className={'links-list'}>
             <li className={'hovered'}><a className={'link'}>Fb</a></li>
             <li className={'hovered'}><a className={'link'}>Ig</a></li>
@@ -18,6 +54,15 @@ const Intro = () => {
           </ul>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={handleOutsideClick}>
+          <div className="modal">
+            <p>Модальное окно</p>
+            <button onClick={handleCloseModal}>Закрыть</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
